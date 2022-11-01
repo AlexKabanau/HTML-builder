d179a33a801fde5749aadb09d7fc1a3a45b24da3
Alexandr Kabanau <Azraill1989@gmail.com>
2022-11-01 22:19:47 +0300

    feat: write-file - done

---
+++ 02-write-file/index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const rl = readline.createInterface({ 
+    input: process.stdin,
+    output: process.stdout
+ });
+//  let data = '';
+
+console.log("Hello, put your test");
+rl.on('line', (input) => {
+  if (input === 'exit') {
+    // console.log("Bye!");
+    rl.close();
+  } else {
+    // console.log(data += input);
+    fs.appendFile(
+      path.join(__dirname, 'mynotes.txt'),
+      `${input}\n`,
+      () => {console.log(`Your text is ${input}`)}
+    );
+  }
+});
+process.on('beforeExit', () => {
+  console.log('Bye!');
+})
+
+// fs.appendFile(
+//   path.join(__dirname, 'notes', 'mynotes.txt'),
+//   ' From append file',
+//   err => {
+//       if (err) throw err;
+//       console.log('Файл был изменен');
+//   }
+// );
+
+// fs.writeFile(
+//     path.join(__dirname, 'text.txt'),
+//     (err) => {
+//         if (err) throw err;
+//         console.log('Файл был создан');
+//     }
+// );
+
